0e6a3b87b1f9b3a51d50c3095ed7c01036307ede
Jonas Lukasczyk <jl@jluk.de>
2022-08-18 16:50:47 +0200

    x

--- packages/main/src/arcCommanderService.ts
+++ packages/main/src/arcCommanderService.ts
@@ -191,7 +191,7 @@ export const ArcCommanderService = {
     );
 
     try{
-      const status = execSync(`'${acPath}' ${params}`, { cwd: config.arc_commander.path }).toString();
+      const status = execSync(`"${acPath}" ${params}`).toString();
       return [1,status];
     } catch (error) {
       return [0,error];
