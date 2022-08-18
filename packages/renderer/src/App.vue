060481eb7be4f90fee8d3333a19db09aa9211bf0
Jonas Lukasczyk <jl@jluk.de>
2022-08-18 17:20:34 +0200

    x

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -308,7 +308,7 @@ const createArc = ()=>{
       queue.push([`-p ${config.arc_name} i create -i ${config.arc_name}`, state_arc]);
       queue.push([`-p ${config.arc_name} a init -a ${config.arc_name}`, state_assay]);
       if(config.user_name && config.user_eMail){
-        queue.push([`-p ${config.arc_name} config setgituser -l -n ${config.user_name} -e ${config.user_eMail}`, null]);
+        queue.push([`-p ${config.arc_name} config setgituser -l -n '${config.user_name}' -e '${config.user_eMail}'`, null]);
       }
       if(config.user_gitlab){
         queue.push([`-p ${config.arc_name} remote token get -s git.nfdi4plants.org`, state_token]);
